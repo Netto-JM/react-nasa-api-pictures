ae66a997d1227b3e536a5c98bc500eec281d2a98
NettoJM <nettojm86@gmail.com>
2022-07-17 15:18:38 -0300

    Updates

---
+++ precache-manifest.4c3e9de975544d3b653a8ac5f0cd5843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecae0193e263086fba3f",
+    "url": "/react-gh-pages/static/css/main.b7c05526.chunk.css"
+  },
+  {
+    "revision": "ecae0193e263086fba3f",
+    "url": "/react-gh-pages/static/js/main.06e2e468.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "b75ba040a3c9cb9f24de",
+    "url": "/react-gh-pages/static/js/2.cafc8819.chunk.js"
+  },
+  {
+    "revision": "db37a521c375b092d3b44eb87e5959f3",
+    "url": "/react-gh-pages/static/media/rocket.db37a521.svg"
+  },
+  {
+    "revision": "88ecf6c3309e16f431c20b3c4785cd80",
+    "url": "/react-gh-pages/index.html"
+  }
+];
